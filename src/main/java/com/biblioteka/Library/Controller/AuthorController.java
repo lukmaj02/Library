4a995aa79d970d8a4c73eb8801e7c1a6de87b5e2
Lukasz Majcher <lmajcher02@gmail.com>
2023-07-04 22:00:22 +0200

    testing

--- src/main/java/com/biblioteka/Library/Controller/AuthorController.java
+++ src/main/java/com/biblioteka/Library/Controller/AuthorController.java
@@ -1,9 +1,12 @@
 package com.biblioteka.Library.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("author")
 public class AuthorController {
+    @GetMapping("")
+    public void getAuthors(){}
 }
